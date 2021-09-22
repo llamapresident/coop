551c63ce8defc131fd4b1f0f215bddbd0c52ee8d
mnava919 <78876687+mnava919@users.noreply.github.com>
2021-09-22 13:51:28 -0700

    WebScraping

--- TextMain.py
+++ TextMain.py
@@ -1,6 +1,6 @@
-import bs4
-import requests
-request = requests.get("https://www.evit.com/high_school_career_training/hs")
-soup = bs4.BeautifulSoup(request.text, 'html.parser')
-print(soup.select('#ctl00_ContentPlaceHolder1_ctl11_divContent > ul'))
-for elem in programNames:
+from requests_html import HTML, HTMLSession
+
+session = HTMLSession()
+r = session.get('https://www.evit.com/high_school_career_training/hs')
+main_content = r.html.find('div.main-content', first=True)
+print(main_content.text)
